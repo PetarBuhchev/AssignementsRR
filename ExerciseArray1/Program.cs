9fb70776171de5e1e1be8a8d7da01e8b512bd493
PetarBuhchev <petarbuhchev@outlook.com>
2023-12-13 18:23:46 +0200

    Homework13.12.2023

---
+++ ExerciseArray1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExerciseArray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+
+            Random r = new Random();
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = r.Next(0, 101);
+            }
+            for (int i = myArray.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
---
+++ ExerciseLists/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExerciseLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                myList.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Chetni");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(myList[i]);
+                }
+            }
+            Console.WriteLine("Nechetni");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(myList[i]);
+                }
+            }
+        }
+    }
+}
