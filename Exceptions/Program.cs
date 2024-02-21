eed9f03de341fe610e943dea8105c4ef6a3c98d6
PetarBuhchev <petarbuhchev@outlook.com>
2024-02-21 21:20:25 +0200

    Exception

---
+++ Exceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FormatException fe = new FormatException("The number is negative!");
+			double v = double.Parse(Console.ReadLine());
+			double t = double.Parse(Console.ReadLine());
+
+            if (v < 0 || t < 0)
+            {
+                throw fe;
+            }
+            else
+            {
+                Console.WriteLine(v*t);
+            }
+        }
+    }
+}
