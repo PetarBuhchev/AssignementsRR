b4da544a615cd24c44b59876cc0157f0ae7d0c20
PetarBuhchev <petarbuhchev@outlook.com>
2024-02-26 21:05:49 +0200

    Change cause of feedback

--- Homework26.02.2024/Program.cs
+++ Homework26.02.2024/Program.cs
@@ -17,6 +17,22 @@ namespace Homework26._02._2024
             {
                 text = Console.ReadLine();
                 avrgGrade = double.Parse(Console.ReadLine());
+
+                using (StreamWriter writer = new StreamWriter("studentFile.txt"))
+                {
+                    writer.Write(text + " ");
+                    writer.Write(avrgGrade);
+                }
+
+                using (StreamReader reader = new StreamReader("studentFile.txt"))
+                {
+
+                    string textRead = reader.ReadLine();
+                    int index = textRead.IndexOf(" ");
+                    string name = (textRead.Substring(0, index)).ToUpper();
+                    double grade = double.Parse(textRead.Substring(index + 1, textRead.Length - index - 1));
+                    Console.WriteLine($"{name} {grade:f4}");
+                }
             }
             catch (FormatException fe)
             {
@@ -31,21 +47,7 @@ namespace Homework26._02._2024
                 Console.WriteLine("Грешка");
             }
 
-            using (StreamWriter writer = new StreamWriter("studentFile.txt"))
-            {
-                writer.Write(text + " ");
-                writer.Write(avrgGrade);
-            }
-
-            using (StreamReader reader = new StreamReader("studentFile.txt"))
-            {
-
-                string textRead = reader.ReadLine();
-                int index = textRead.IndexOf(" ");
-                string name = (textRead.Substring(0, index)).ToUpper();
-                double grade = double.Parse(textRead.Substring(index + 1, textRead.Length - index - 1));
-                Console.WriteLine($"{name} {grade:f4}");
-            }
+            
         }
     }
 }
