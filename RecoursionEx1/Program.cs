98383853c7c1a709417b513714544eeb8d55014e
PetarBuhchev <petarbuhchev@outlook.com>
2024-03-06 09:15:34 +0200

    NOD

---
+++ RecoursionEx1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoursionEx1
+{
+    internal class Program
+    {
+        static int NODiterative(int a, int b)
+        {
+            int r;
+            if (b < a)
+            {
+                while (b != 0)
+                {
+                    r = a % b;
+                    a = b; b = r;
+                }
+                return a;
+            }
+            else
+            {
+                while (a != 0)
+                {
+                    r = b % a;
+                    b = a; a = r;
+                }
+                return b;
+            }
+        }
+        static int NODrecoursive(int a, int b)
+        {
+            if (b == 0) return a;
+            return NODrecoursive(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+        }
+    }
+}
