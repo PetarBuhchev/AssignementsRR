b45bc6be849fdfafe09075db82acb7eb3e684cab
PetarBuhchev <petarbuhchev@outlook.com>
2024-03-12 10:02:06 +0200

    FinishedEx4

--- Ex4.HW08.03.2024/Program.cs
+++ Ex4.HW08.03.2024/Program.cs
@@ -10,7 +10,22 @@ namespace Ex4.HW08._03._2024
     {
         static int BinomIt(int n, int k)
         {
-
+            int num = 1;
+            for (int i = n; i >= 1; i--)
+            {
+                num *= i;
+            }
+            int denum = 1;
+            for (int k1 = k; k1 >= 1; k1--)
+            {
+                denum *= k1;
+            }
+            for (int k2 = n-k; k2 >= 1; k2--)
+            {
+                denum *= k2;
+            }
+            int result = num / denum;
+            return result;
         }
         static int BinomRec(int n, int k)
         {
@@ -21,6 +36,9 @@ namespace Ex4.HW08._03._2024
         {
             int n = int.Parse(Console.ReadLine());
             int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinomIt(n,k));
+            Console.WriteLine(BinomRec(n,k));
         }
     }
 }
