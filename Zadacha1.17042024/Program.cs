298f2e2146e473530c007ca7994b7f4b41fb88f0
PetarBuhchev <petarbuhchev@outlook.com>
2024-04-17 20:56:16 +0300

    HW 17042024

---
+++ Zadacha1.17042024/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1._17042024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int count = 0;
+            int count1 = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 'A' && input[i] <= 'Z' || input[i] >= 'a' && input[i] <= 'z')
+                {
+                    count += input[i];
+                }
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    count1 += input[i];
+                }
+            }
+            Console.WriteLine(count);
+            Console.WriteLine(count1);
+        }
+    }
+}
---
+++ Zadacha2_17042024/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2_17042024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char s = char.Parse(Console.ReadLine());
+            int count = input.Count(x => x == s);
+            if (count == 0)
+            {
+                Console.WriteLine("Няма такъв символ във въведения текст!");
+                return;
+            }
+            int[] arr = new int[count];
+
+            int index = 0;
+            int arrIndex = 0;
+            while (index != -1) 
+            {
+                arr[arrIndex] = index;
+                arrIndex++;
+                index = input.IndexOf(s, index+1);
+            }
+
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
