1f7d6e2aab4f51ff5038f244edef0beacb35c997
PetarBuhchev <petarbuhchev@outlook.com>
2024-05-28 07:07:13 +0300

    HW28052024

---
+++ HW_28052024/Polinom.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_28052024
+{
+    internal class Polinom
+    {
+        private int n;
+        private double[] coefficients;
+        public Polinom(int n)
+        {
+            this.n = n;
+            this.coefficients = new double[n + 1];
+        }
+        public void InputCoefficients()
+        {
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                Console.WriteLine($"Enter a coefficient {i}: ");
+                this.coefficients[i] = double.Parse(Console.ReadLine());
+            }
+        }
+        public int GetCoefficientsLength()
+        {
+            return this.coefficients.Length;
+        }
+        public double CalculateIt(double x)
+        {
+            double res = 0;
+            for (int i = coefficients.Length-1, j = n; i >= 0; i--, j--)
+            {
+                if (i == coefficients.Length-1)
+                {
+                    res += Math.Pow(coefficients[i],j);
+                }
+                else if (i == 0)
+                {
+                    res += 1;
+                }
+                res *= Math.Pow(coefficients[i],j) + x; 
+            }
+            return res;
+        }
+        public double CalculateRec(double x, int n, int index)
+        {
+            if (index < 0)
+            {
+                return 0;
+            }
+            else if (index == 0)
+            {
+                return 1 + CalculateRec(x, n - 1, index - 1);
+            }
+            else if (index == coefficients.Length-1)
+            {
+                return Math.Pow(coefficients[index], n) + CalculateRec(x, n - 1, index - 1);
+            }
+            return (Math.Pow(coefficients[index], n) + x) * CalculateRec(x, n - 1, index - 1);
+        }
+
+    }
+}
---
+++ HW_28052024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_28052024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            Polinom polinom = new Polinom(n);
+            polinom.InputCoefficients();
+            Console.Write("Enter x1: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter x2: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Step: ");
+            double step = double.Parse(Console.ReadLine());
+
+            for (double x = x1; x <= x2; x += step)
+            {
+                Console.WriteLine($"x = {x}\t\t{polinom.CalculateIt(x)}\t\t {polinom.CalculateRec(x, n, polinom.GetCoefficientsLength())}");
+            }
+        }
+    }
+}
