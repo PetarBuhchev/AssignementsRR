03a6b7961eb8a0bea56b076d6d8ee125c6a72aa5
PetarBuhchev <petarbuhchev@outlook.com>
2024-05-30 08:34:16 +0300

    HW_30052024

---
+++ HW30052024/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW30052024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            int[] grades = new int[5];
+            int index = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    grades[index++] = int.Parse(list[i].ToString());
+                }
+            }
+
+            Array.Sort(grades);
+
+            int outputIndex = 0;
+            if (grades[3] == grades[4])
+            {
+                outputIndex = list.LastIndexOf(grades[4].ToString()) - 1;
+            }
+            else
+            {
+                outputIndex = list.IndexOf(grades[4].ToString()) - 1;
+            }
+
+            Console.WriteLine(list[outputIndex].ToString());
+        }
+    }
+}
