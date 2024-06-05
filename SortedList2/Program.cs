19d8a2bc4ccbf175c713df2f9e35e01e245f9602
PetarBuhchev <petarbuhchev@outlook.com>
2024-06-05 22:46:56 +0300

    SortedList2

---
+++ SortedList2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, List<string>> sl = new SortedList<string, List<string>>();
+
+            string country = Console.ReadLine();
+
+            while (country != "end")
+            {
+                sl.Add(country, Console.ReadLine().Split().ToList());
+                country = Console.ReadLine();
+            }
+
+            foreach (var item in sl)
+            {
+                if (item.Key.Length > 6)
+                {
+                    Console.Write(item.Key + " - ");
+                    foreach (var item2 in item.Value) 
+                    {
+                        Console.Write(item2 + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
